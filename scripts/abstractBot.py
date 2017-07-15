29029cb9d19fac4554eddd65a58119acc88dfc67
spw3491 <bpbryl.spw3491@gmail.com>
2017-07-15 16:08:32 +0900

    ishigami respect

--- scripts/abstractBot.py
+++ scripts/abstractBot.py
@@ -6,9 +6,11 @@ from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
 from gazebo_msgs.msg import ModelStates 
 from sensor_msgs.msg import Image
+import message_filters
+
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-
+import numpy as np
 
 class AbstractBot(object):
     __metaclass__ = ABCMeta
@@ -34,7 +36,37 @@ class AbstractBot(object):
 
         # camera subscriver
         # please uncoment out if you use camera
-        self.image_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.imageCallback)
+        # get rgb and depth image  http://answers.ros.org/question/219029/getting-depth-information-from-point-using-python/
+        self.image_sub = message_filters.Subscriber("camera/rgb/image_raw", Image)
+        self.depth_sub = message_filters.Subscriber("camera/depth/image_raw", Image)
+
+        # ApproximateTimeSynchronizer http://docs.ros.org/api/message_filters/html/python/
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.depth_sub], 1, 0.1)
+        self.ts.registerCallback(self.imageDepthcallback)
+
+        # view gui flag
+        self.cv_view = True
+
+        # red region near position(loc) and distance(val)
+        self.red_loc = np.zeros((2))
+        self.red_val = np.array([0.0])
+
+        # yellow region near position(loc) and distance(val)
+        self.yel_loc = np.zeros((2))
+        self.yel_val = np.array([0.0])
+
+        # center position and max value
+        self.loc_centor_x  = np.array([1])
+        self.loc_max       = np.zeros((2))
+
+        # average depth
+        self.ave_val = np.array([0.0])
+        self.M_val = np.array([0.0])
+        self.L_val = np.array([0.0])
+        self.R_val = np.array([0.0])        
+
+        # step
+        self.step = np.array([0])
 
     # bumper topic call back sample
     # update bumper state
@@ -59,14 +91,135 @@ class AbstractBot(object):
 
     # camera image call back sample
     # comvert image topic to opencv object and show
-    def imageCallback(self, data):
+    def imageDepthcallback(self, rgb_data, depth_data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
+            rgb_image   = self.bridge.imgmsg_to_cv2(rgb_data,   "bgr8")
 
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
+            # resize  http://tatabox.hatenablog.com/entry/2013/07/15/164015
+            h = rgb_image.shape[0]
+            w = rgb_image.shape[1]            
+            rgb_image_ = cv2.resize(rgb_image,(w/4,h/4))
+
+            if w <= 0:
+                self.loc_centor_x = 640/8;
+                self.loc_max[0]   = 640/4;
+                self.loc_max[1]   = 480/4;                                
+            else:
+                self.loc_centor_x = w/8;
+                self.loc_max[0]   = w/4;
+                self.loc_max[1]   = h/4;
+
+            # reffer http://answers.ros.org/question/58902/how-to-store-the-depth-data-from-kinectcameradepth_registreredimage_raw-as-gray-scale-image/
+            #depth_image = self.bridge.imgmsg_to_cv2(depth_data, "32FC1")            
+            depth_image = self.bridge.imgmsg_to_cv2(depth_data, "passthrough")
+
+            # resize  http://tatabox.hatenablog.com/entry/2013/07/15/164015            
+            h = depth_image.shape[0]
+            w = depth_image.shape[1]
+            depth_image_ = cv2.resize(depth_image,(w/4,h/4))            
+        except CvBridgeError, e:
+            print e
+
+        #depth_array = np.array(depth_image, dtype=np.float32)
+        depth_array = np.array(depth_image_, dtype=np.float32)        
+        cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
+
+        #get middle average depth
+        #get 1row    http://qiita.com/supersaiakujin/items/d63c73bb7b5aac43898a
+        #cal average http://programming.blogo.jp/python/average 
+
+        #mid = depth_array[60,:]
+        #if 0 < len(mid) and sum(mid) != 0:
+        #    self.ave_val = sum(mid) / len(mid)
+        #else:
+        #    self.ave_val = 0
+
+        # array access  http://qiita.com/supersaiakujin/items/d63c73bb7b5aac43898a
+        
+        self.L_val = depth_array[ 60,  2]
+        self.M_val = depth_array[ 60, 80]
+        self.R_val = depth_array[ 60,158]
+
+        # --- test Red Region ---
+        # https://www.blog.umentu.work/python3-opencv3%E3%81%A7%E6%8C%87%E5%AE%9A%E3%81%97%E3%81%9F%E8%89%B2%E3%81%AE%E3%81%BF%E3%82%92%E6%8A%BD%E5%87%BA%E3%81%97%E3%81%A6%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B%E3%80%90%E5%8B%95%E7%94%BB/
+        # BGR array
+        # extract red region
+        lower_red = np.array([0,    0,   64])
+        upper_red = np.array([8,    8,  255])       
+        red_mask = cv2.inRange(rgb_image_, lower_red, upper_red)
+        red_image = cv2.bitwise_and(rgb_image_, rgb_image_,   mask=red_mask) 
+        red_depth = cv2.bitwise_and(depth_array, depth_array, mask=red_mask)
+
+        # extract yellow region
+        lower_yel = np.array([0,    64,   64])
+        upper_yel = np.array([8,    255,  255])       
+        yel_mask = cv2.inRange(rgb_image_, lower_yel, upper_yel)
+        yel_image = cv2.bitwise_and(rgb_image_, rgb_image_,   mask=yel_mask)
+        yel_depth = cv2.bitwise_and(depth_array, depth_array, mask=yel_mask)        
+
+        # extract depth region
+        # lower_dep = 0.0
+        # upper_dep = 0.2      
+        # dep_mask = cv2.inRange(depth_array, lower_dep, upper_dep)
+        # dep_image = cv2.bitwise_and(rgb_image_, rgb_image_, mask=dep_mask)
+
+        # extract max value
+        # http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_contours/py_contour_properties/py_contour_properties.html#contour-properties
+        # --- search red near area ---
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(red_depth, mask=red_mask)
+        self.red_loc = min_loc
+        self.red_val = min_val
+        # if not exit max_loc = [-1, -1]
+        #cv2.circle(red_image, self.red_loc, 3, (255, 255, 255), -1)
+        r = int((1.0 - self.red_val)*10.0)
+        if r < 0 :
+            r = 0
+        if min_loc[0] == -1 and min_loc[1] == -1 :
+            r = 0
+        cv2.circle(red_image, self.red_loc, r, (255, 255, 255), -1)        
+
+        # --- search yellow near area ---
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(yel_depth, mask=yel_mask)
+        self.yel_loc = min_loc
+        self.yel_val = min_val
+        # if not exit max_loc = [-1, -1]        
+        # cv2.circle(yel_image, self.yel_loc, 3, (255, 255, 255), -1)
+        r = int((1.0 - self.yel_val)*10.0)
+        if r < 0 :
+            r = 0
+        if min_loc[0] == -1 and min_loc[1] == -1 :
+            r = 0            
+        cv2.circle(yel_image, self.yel_loc, r, (255, 255, 255), -1)
+
+        # --- debug ---------------------------------------------
+        # step5 (bumper white)
+        if self.step == 5:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (255, 0, 0), 3)
+        # step6 (near wall)
+        if self.step == 6:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (255, 255, 0), 3)            
+        # step1 (yellow)            
+        elif self.step == 1:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (0, 255, 255), 3)
+        # step2 (red)            
+        elif self.step == 2:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (0, 0, 255), 3)
+        # step3 (turn)            
+        elif self.step == 3:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (0, 255, 0), 3) 
+        # step4 (random white)            
+        elif self.step == 4:
+            cv2.rectangle(rgb_image_, (0,0), (160,120), (255, 255, 255), 3)
+
+        if self.cv_view == True:
+            cv2.imshow("Image window", rgb_image_)            
+            cv2.imshow("Depth window", depth_array)
+            cv2.imshow("Red Image window", red_image)
+            #cv2.imshow("Red Depth window", red_depth)
+            cv2.imshow("Yellow Image window", yel_image)
+            #cv2.imshow("Yellow Depth window", yel_depth)                        
+            #cv2.imshow("near depth Image window", dep_image)                        
+            cv2.waitKey(1)
 
     @abstractmethod
     def strategy(self):
--- scripts/randomBot.py
+++ scripts/randomBot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import rospy
 import rospkg
 import random
@@ -12,66 +13,239 @@ class RandomBot(AbstractBot):
     
     def strategy(self):
         r = rospy.Rate(100)
-        
+
+        #target        
         target_speed = 0
-        target_turn = 0
+        target_turn  = 0
+
+        #control
         control_speed = 0
         control_turn = 0
 
-        surplus = 0
+        #max
+        max_control_speed =  0.5
+        min_control_speed = -0.5
 
-        UPDATE_FREQUENCY = 1
+        #time
+        #UPDATE_FREQUENCY = 1
+        UPDATE_FREQUENCY = 0.1
         update_time = 0
 
+        #flag
+        bum_flag = False
+        yel_flag = False        
+        red_flag = False
+        rot_flag = False
+
+        th = 0.0
+        sp = 0.0
+
         while not rospy.is_shutdown():
+            # step5: bumper            
             if self.center_bumper or self.left_bumper or self.right_bumper:
+                self.step = 5                                
                 update_time = time.time()
-                rospy.loginfo('bumper hit!!')
-                x = 0
-                th = 3
-                control_speed = -1
-                control_turn = 0
-            
+                rospy.loginfo('bumper hit!!')                       
+
+                control_speed = -0.05
+                target_speed  = -0.05
+
+                #control_turn = 1
+                #target_turn  = 1
+
+                if self.L_val < self.R_val:
+		#control_turn  = 1.0                    
+		     target_turn  = 1.0
+                else:
+                    target_turn  = -1.0 
+
+                # init
+                th_cnt = 0.0
+                bum_flag = True                
+                yel_flag = False
+                red_flag = False
+                rot_flag = False
+
+            # --- use rgb and depth data ---
+            # step6: neal wall
+            #elif 0.01 > self.L_val or 0.01 > self.R_val or 0.01 > self.M_val:
+            elif 0.05 > self.M_val:    
+                self.step = 6                
+                update_time = time.time()
+                rospy.loginfo('near wall ---- !!')
+
+                control_speed = 0
+                target_speed  = 0
+
+                #if self.L_val > self.R_val:
+                control_turn = -1.0
+                target_turn  = -1.0
+                #else:
+                #    control_turn = -1.0
+                #    target_turn  = -1.0
+
+                # init
+                th_cnt = 0.0
+                bum_flag = True                
+                yel_flag = False
+                red_flag = False
+                rot_flag = False                
+
             elif time.time() - update_time > UPDATE_FREQUENCY:
                 update_time = time.time()
                 
-                value = random.randint(1,1000)
-                if value < 500:
-                    x = 1
-                    th = 0
+                # --- use rgb and depth data ---
+                # step1: extract yellow (yellow)
+                if self.yel_loc[0] != -1 and self.yel_loc[1] != -1:
+                    self.step = 1
 
-                elif value < 750:
-                    x = 0
-                    th = 3
+                    # init
+                    th_cnt = 0.0
+                    if bum_flag:
+                        target_turn  = 0
+                        control_turn = 0
+                        #target_speed  = max_control_speed
+                        #control_speed = max_control_speed
+                        bum_flag = False                        
+                    red_flag = False
+                    yel_flag = True
+                    if rot_flag:
+                        target_turn  = 0
+                        control_turn = 0
+                        #target_speed  = max_control_speed
+                        #control_speed = max_control_speed
+                        rot_flag = False
 
-                elif value < 1000:
-                    x = 0
-                    th = -3
+                    loc_x = self.yel_loc[0]
+                    cen_x = self.loc_centor_x
+                    val   = self.red_val
+
+                    th = 0.5*np.arctan((1.5*(loc_x - cen_x)/cen_x))
+                    target_turn  = -th
+
+                    if   0.01 < val:
+                        sp = 0.50
+                    else:
+                        sp = 0.48
+                    target_speed = sp
+                    
+                # step2: extract red (red)
+                elif self.red_loc[0] != -1 and self.red_loc[1] != -1:
+                    self.step = 2
+                    
+                    # init
+                    th_cnt = 0.0
+                    if bum_flag:
+                        target_turn  = 0
+                        control_turn = 0
+                        #target_speed  = max_control_speed
+                        #control_speed = max_control_speed
+                        bum_flag = False                                            
+                    yel_flag = False
+                    red_flag = True
+                    if rot_flag:
+                        target_turn  = 0
+                        control_turn = 0
+                        #target_speed  = max_control_speed
+                        #control_speed = max_control_speed
+                        rot_flag = False
+
+                    loc_x = self.red_loc[0]
+                    cen_x = self.loc_centor_x
+                    val   = self.red_val
+
+                    th = 0.5*np.arctan((1.5*(loc_x - cen_x)/cen_x))
+                    target_turn  = -th
+
+                    if   0.01 < val:
+                        sp = 0.50
+                    else:
+                        sp = 0.48
+                    target_speed = sp
+
+		    #print('loc_x:%f cen_x:%f' %(loc_x, cen_x))
+
+                # step3: check total cnt of rotation th (green)
+                elif th_cnt < 15 and (yel_flag or red_flag):
+		#elif yel_flag or red_flag:
+                    self.step = 3
+
+                    #init
+                    rot_flag = True
+
+                    #control_speed = 0
+                    target_speed  = -0.1
+                    #target_speed  = target_speed - 0.2
+
+                    #control_turn = 1.5
+                    if self.L_val > self.R_val:
+                        target_turn  = 1.0
+                    else:
+                        target_turn  = -1.0                        
+                    th_cnt = th_cnt +1
+
+                # step4: random (while)
                 else:
-                    x = 0
-                    th = 0
+                    self.step = 4
+                    
+                    # init
+                    th_cnt =   th_cnt - 5
+                    if th_cnt < 0:
+                        th_cnt = 0
 
-            target_speed = x
-            target_turn = th
+                    yel_flag = False
+                    red_flag = False
+                    rot_flag = False                    
+
+                    # random  http://www.python-izm.com/contents/application/random.shtml
+                    #rand = random.uniform(0.0, 1.0)
+                    #if   0.75 < rand:
+                    #    target_speed  = -0.5
+                    #    target_turn   = -3
+                    #elif 0.50 < rand:
+                    #    target_speed  = 0.5
+                    #    target_turn   = -3
+                    #elif 0.25 < rand:
+                    #    target_speed  = 0.5
+                    #    target_turn   = -3
+                    #else:
+                    #    target_speed  = 0.5
+                    #    target_turn   = 3
+
+		    target_speed  = 0.5
+                    target_turn   = -3
+
+            #print http://programming-study.com/technology/python-print/
+            #print('speed(%f, %f)_turn(%f, %f)_th(%f)_sp(%f)_(%f, %f, %f)_ave(%f)' %(target_speed, control_speed, target_turn, control_turn, th, sp, self.L_val, self.M_val, self.R_val, self.ave_val))
+	    print('speed(%f, %f)_turn(%f, %f)' %(target_speed, control_speed, target_turn, control_turn))
+
+            # def 0.02
+            del_speed  = 0.1
+            # def 0.1
+            del_turn   = 0.2
 
             if target_speed > control_speed:
-                control_speed = min( target_speed, control_speed + 0.02 )
+                control_speed = min( target_speed, control_speed + del_speed )
             elif target_speed < control_speed:
-                control_speed = max( target_speed, control_speed - 0.02 )
+                control_speed = max( target_speed, control_speed - del_speed )
             else:
                 control_speed = target_speed
 
+            #max speed check
+            control_speed = min (control_speed, max_control_speed)
+            control_speed = max (control_speed, min_control_speed)
+
             if target_turn > control_turn:
-                control_turn = min( target_turn, control_turn + 0.1 )
+                control_turn = min( target_turn, control_turn + del_turn )
             elif target_turn < control_turn:
-                control_turn = max( target_turn, control_turn - 0.1 )
+                control_turn = max( target_turn, control_turn - del_turn)
             else:
-                control_turn = target_turn
+                control_turn = target_turn  
 
             twist = Twist()
             twist.linear.x = control_speed; twist.linear.y = 0; twist.linear.z = 0
             twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = control_turn
-            print(twist)
+            #print(twist)
         
             self.vel_pub.publish(twist)
 
